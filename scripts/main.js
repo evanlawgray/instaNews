dbf3dd2c1d38d02dd993d730f143875442c2c001
Evan Gray <evanlawgray@gmail.com>
2017-01-24 23:51:48 -0800

    Improve dashboard animation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,13 @@
 $(function(){
 
 	var $target = $('#target');
-	var $contentContainer = $('.content');
+	//var $contentContainer = $('.content');
 	var $dashboard = $('.dashboard');
+	var $logoContainer = $('.times-logo-container');
 
 	$target.change(function() {
-		$dashboard.animate({
-			position: 'absolute',
-			height: '20vh',
-			top: 0,
-			left: 0
-		}, 1000, 'linear', function(){
-			return false;
+		$dashboard.animate({position: 'absolute', height: '15vh',top: 0,left: 0}, 200, 'linear');
+		$logoContainer.animate({width:'70px'}, 200, 'linear');
 		});
-	});
+	/*});*/
 });
