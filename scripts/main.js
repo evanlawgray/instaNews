af8130fda6d9979fee38a948fa700376e9fee9e7
Evan Gray <evanlawgray@gmail.com>
2017-01-25 22:11:55 -0800

    Rough draft API request.

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,21 @@ $(function(){
 	var $logo = $( '.times-logo' );
 
 	$target.change(function() {
-		var $section = $( "select option:selected" ).val();
+		var $selectedTopic = $( "select option:selected" ).val();
 
 		$logoContainer.animate({height: '25vh'}, 200, 'linear');
 		$dashboard.animate({position: 'absolute', height: 'auto','max-width': '700px', top: 0,left: 0}, 200, 'linear');
 		//$logo.animate({width:'25%', height: '25%'}, 200, 'linear');
 		$storiesGrid.empty().animate({height: '75vh', display: 'block'});
+		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+		url += '?' + $.param({'key': '4bd2bd098b3449068be47890b4f42e24','q': $selectedTopic});
+		$.ajax({
+			url: url,
+			method: 'GET' 
+		}).done(function(data){
+			console.log(data);
+
+			});
+
 		});
 });
