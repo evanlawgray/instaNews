4bf1dde17a468e9c883c1434379b78e805631f89
Evan Gray <evanlawgray@gmail.com>
2017-01-25 18:42:50 -0800

    Add tablet breakpoint.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 $(function(){
 
 	var $target = $('#target');
-	//var $contentContainer = $('.content');
+	var $storiesGrid = $('.stories-grid');
 	var $dashboard = $('.dashboard');
 	var $logoContainer = $('.times-logo-container');
 
 	$target.change(function() {
 		$dashboard.animate({position: 'absolute', height: '15vh',top: 0,left: 0}, 200, 'linear');
 		$logoContainer.animate({width:'70px'}, 200, 'linear');
+		$storiesGrid.empty().animate({height: '75vh'})
 		});
 	/*});*/
 });
