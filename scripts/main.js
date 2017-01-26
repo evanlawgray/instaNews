d6ea925a5db33e66dc3c144907c21207c5808cd4
Evan Gray <evanlawgray@gmail.com>
2017-01-25 21:50:08 -0800

    Remove unneeded stories grid markup.

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,14 @@ $(function(){
 	var $storiesGrid = $('.stories-grid');
 	var $dashboard = $('.dashboard');
 	var $logoContainer = $('.times-logo-container');
+	var $logo = $( '.times-logo' );
 
 	$target.change(function() {
-		$dashboard.animate({position: 'absolute', height: '15vh',top: 0,left: 0}, 200, 'linear');
-		$logoContainer.animate({width:'70px'}, 200, 'linear');
-		$storiesGrid.empty().animate({height: '75vh'})
+		var $section = $( "select option:selected" ).val();
+
+		$logoContainer.animate({height: '25vh'}, 200, 'linear');
+		$dashboard.animate({position: 'absolute', height: 'auto','max-width': '700px', top: 0,left: 0}, 200, 'linear');
+		//$logo.animate({width:'25%', height: '25%'}, 200, 'linear');
+		$storiesGrid.empty().animate({height: '75vh', display: 'block'});
 		});
-	/*});*/
 });
