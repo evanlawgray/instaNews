2f30b0929604799a75fef4af296e951d6cff08df
Evan Gray <evanlawgray@gmail.com>
2017-01-26 23:49:38 -0800

    Improve header animation.

--- scripts/main.js
+++ scripts/main.js
@@ -11,15 +11,16 @@ $(function(){
 		$selectedTopic = $( 'select option:selected' ).val();
 
 //Shrink dashboard and move it to top of page
+		var currHeight = $dashboard.height();
+		var autoHeight = $dashboard.css('height', 'auto').height();
+		$dashboard.height(currHeight).animate({height: autoHeight, position: 'absolute', top: 0, left: 0}, 100, 'linear');
 
-		$logoContainer.animate({height: '25vh'}, 200, 'linear');
-		$dashboard.animate({position: 'absolute', height: 'auto','max-width': '700px', top: 0,left: 0}, 200, 'linear');
+		$logoContainer.animate({width: '20vh'}, 100, 'linear');
+		/*$dashboard.animate({position: 'absolute', height: 'auto','max-width': '700px', top: 0,left: 0}, 200, 'linear');*/
 		//$logo.animate({width:'25%', height: '25%'}, 200, 'linear');
 		$storiesGrid.empty().animate({height: '75vh'});
 
 		var apiLink = "https://api.nytimes.com/svc/topstories/v2/";
-		var storiesArray = [];
-		var response = {};
 		
 		$.ajax({
 			url: apiLink += $selectedTopic + '.json' + '?' + $.param({'api-key': '4bd2bd098b3449068be47890b4f42e24'}),
