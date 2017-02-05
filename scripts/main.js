a56434d16d1a7ad8616cd79a6fb66b83410d1044
Evan Gray <evanlawgray@gmail.com>
2017-02-04 22:51:01 -0800

    Add conditional to stop users from erroring out by selecting 'Sections...'

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,10 @@ $(function(){
 		$selectedTopic = $( 'select option:selected' ).val();
 		var apiLink = 'https://api.nytimes.com/svc/topstories/v2/';
 
+//Make sure selected topic is valid
+
+		if ($selectedTopic !== 'sections') {
+
 //Shrink dashboard and move it to top of page (and append loading spinner gif)
 		
 		$dashboard.addClass('dash-to-top');
@@ -27,7 +31,6 @@ $(function(){
 			method: 'GET' 
 		}).done(function(data){
 			var storiesGridItem = '';
-		/*	var storiesFetched = 0;*/
 
 			$storiesGrid.empty();
 
@@ -39,7 +42,6 @@ $(function(){
 
 			$animationSlider.animate({height: '0px'}, 400, 'swing');
 
-	/*		$animationSlider.slideToggle();*/
 			$.each(filteredStoriesArray, function(i, val){
 
 					storiesGridItem = '';
@@ -48,13 +50,13 @@ $(function(){
 					storiesGridItem += '<a href="' + val.url + '">';
 					storiesGridItem += '<p class="story-abstract">' + val.abstract + '</p> </a>' + '</li>';
 					$storiesGrid.append(storiesGridItem);
-				/*	storiesFetched++;*/
+
 			});
 		}).fail(function(){
 
 //Delete loading gif and append div with error message if ajax request fails
 
 			$storiesGrid.empty().append('<div class="error-message"><p>Something went wrong. Please try again later</p></div>');
-		});
+		});} else {return false;}
 	});
 });
